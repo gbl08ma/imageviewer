1245356ebc5590ab8d3840a308d6d25039858a28
gbl08ma <gbl08ma@gmail.com>
2014-08-25 17:04:28 +0100

    Update about screen and copyright messages.

--- src/aboutGUI.cpp
+++ src/aboutGUI.cpp
@@ -92,46 +92,53 @@ void showAbout() {
  
   textArea text;
 
-  textElement elem[10];
+  textElement elem[12];
   text.elements = elem;
 
-  elem[0].text = (char*)"This is a JPEG image viewer for the Casio Prizm calculators. Only baseline JPEG is supported, Progressive and Lossless JPEG is not supported.";
+  elem[0].text = (char*)"This is a JPEG and PNG image viewer for the Casio Prizm calculators. For JPEG images, only baseline is supported, Progressive and Lossless JPEG is not supported. Interlaced PNG is not supported.";
 
   elem[1].newLine = 1;
   elem[1].lineSpacing = 8;
-  elem[1].text = (char*)"Uses the TJpgDec library  - (C)ChaN, 2012.";
+  elem[1].text = (char*)"Uses TJpgDec - (C)ChaN, 2012.";
   
   elem[2].newLine = 1;
-  elem[2].text = (char*)"This image viewer is free software distributed under the following license:";
+  elem[2].text = (char*)"Uses zlib - Copyright (C) 1995-2013 Jean-loup Gailly and Mark Adler";
   
   elem[3].newLine = 1;
-  elem[3].lineSpacing = 8;
-  elem[3].text = (char*)"Copyright (C) 2014, Gabriel Maia and the tny. internet media group";
+  elem[3].text = (char*)"Uses libpng - Copyright (c) 2004, 2006-2014 Glenn Randers-Pehrson";
   
   elem[4].newLine = 1;
-  elem[4].text = (char*)"All rights reserved.";
+  elem[4].lineSpacing = 8;
+  elem[4].text = (char*)"This image viewer is free software distributed under the following license:";
   
   elem[5].newLine = 1;
   elem[5].lineSpacing = 8;
-  elem[5].text = (char*)"Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:";
+  elem[5].text = (char*)"Copyright (C) 2014, Gabriel Maia and the tny. internet media group";
   
   elem[6].newLine = 1;
-  elem[6].lineSpacing = 8;
-  elem[6].text = (char*)"1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.";
+  elem[6].text = (char*)"All rights reserved.";
   
   elem[7].newLine = 1;
   elem[7].lineSpacing = 8;
-  elem[7].text = (char*)"2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.";
+  elem[7].text = (char*)"Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:";
   
   elem[8].newLine = 1;
   elem[8].lineSpacing = 8;
-  elem[8].text = (char*)"THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.";
+  elem[8].text = (char*)"1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.";
   
   elem[9].newLine = 1;
-  elem[9].lineSpacing = 10;
-  elem[9].text = (char*)"Press EXIT";
+  elem[9].lineSpacing = 8;
+  elem[9].text = (char*)"2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.";
+  
+  elem[10].newLine = 1;
+  elem[10].lineSpacing = 8;
+  elem[10].text = (char*)"THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.";
+  
+  elem[11].newLine = 1;
+  elem[11].lineSpacing = 10;
+  elem[11].text = (char*)"Press EXIT";
 
-  text.numelements = 10;
+  text.numelements = 12;
   doTextArea(&text);
- DefineStatusMessage((char*)"", 1, 0, 0);
+  DefineStatusMessage((char*)"", 1, 0, 0);
 }
